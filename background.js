132210a4f5e12d97cce7ac0e9470da1a96dbb213
Andrew Timberlake <andrew@andrewtimberlake.com>
2023-08-07 15:55:17 +0200

    Add context menu support

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+// On installation
+chrome.runtime.onInstalled.addListener(function () {
+  // setup context menu items
+  const isMac = navigator.platform?.toUpperCase().indexOf('MAC') >= 0;
+  let shortcut = isMac ? '⌃ ⌥ C' : 'Ctrl+Alt+C';
+  chrome.contextMenus.create({
+    title: `Copy Markdown URL (${shortcut})`,
+    id: 'copyMarkdown',
+    contexts: ['selection'],
+  });
+  shortcut = isMac ? '⌃ ⇧ C' : 'Ctrl+Shift+C';
+  chrome.contextMenus.create({
+    title: `Copy HTML URL (${shortcut})`,
+    id: 'copyHTML',
+    contexts: ['selection'],
+  });
+});
+
+// On key commands (setup in manifest.json)
 chrome.commands.onCommand.addListener(function (command) {
   if (command === 'copyMarkdown') {
     copyLink('markdown');
@@ -7,12 +26,28 @@ chrome.commands.onCommand.addListener(function (command) {
   }
 });
 
-async function copyLink(linkFormat) {
-  console.log(`copyLink()`, arguments);
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
+// On context menu click
+chrome.contextMenus.onClicked.addListener(function (clickData, tab) {
+  const { menuItemId, pageUrl, selectionText } = clickData;
+
+  if (menuItemId === 'copyMarkdown') {
+    copyLink('markdown', tab);
+  }
+  if (menuItemId === 'copyHTML') {
+    copyLink('html', tab);
+  }
+});
+
+// Get the tab if not supplied and execute script to copy the selected text and url to the clipboard. It has to be executed through a script because the clipboard API is not available in a service worker.
+async function copyLink(linkFormat, tab) {
+  console.log('tab', tab);
+  if (!tab) {
+    [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+  }
+  console.log('tab', tab);
   chrome.scripting.executeScript(
     {
       args: [linkFormat],
@@ -23,6 +58,7 @@ async function copyLink(linkFormat) {
   );
 }
 
+// The actual function that copies the selected text and url to the clipboard
 function doCopy(linkFormat) {
   var selectedText = window.getSelection().toString();
   // console.log('selectedText', selectedText);
